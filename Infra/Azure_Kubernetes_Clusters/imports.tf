679d1ce8ea7a41f790c3b134dd891b46039c1302
Hemanthsoorya <Hemanthsurya.p@gmail.com>
2026-03-03 12:06:38 +0530

    Aiops-v 0.07

--- Infra/Azure_Kubernetes_Clusters/imports.tf
+++ Infra/Azure_Kubernetes_Clusters/imports.tf
@@ -7,15 +7,15 @@
 #  id = "/subscriptions/3d5f5fa8-f989-49ca-b466-b80eff5c9774/resourceGroups/RG-AI-Service-Resources"
 #}
 
-#import {
-#  to = azurerm_resource_group.rg-app-service
-#  id = "/subscriptions/3d5f5fa8-f989-49ca-b466-b80eff5c9774/resourceGroups/RG-DSCU-app-service-00003"
-#}
+import {
+  to = azurerm_resource_group.rg-app-service
+  id = "/subscriptions/3d5f5fa8-f989-49ca-b466-b80eff5c9774/resourceGroups/RG-DSCU-app-service-00003"
+}
 
-#import {
-#  to = azurerm_kubernetes_cluster.aks
-#  id = "/subscriptions/3d5f5fa8-f989-49ca-b466-b80eff5c9774/resourceGroups/RG-DSCU-SecCluster-03/providers/Microsoft.ContainerService/managedClusters/AKS-DSCU-SecCluster"
-#}
+import {
+  to = azurerm_kubernetes_cluster.aks
+  id = "/subscriptions/3d5f5fa8-f989-49ca-b466-b80eff5c9774/resourceGroups/RG-DSCU-SecCluster-03/providers/Microsoft.ContainerService/managedClusters/AKS-DSCU-SecCluster"
+}
 
 #import {
 #  to = azurerm_cognitive_account.ai-service-OpenAIs
